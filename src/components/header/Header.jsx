c39eaaa7af78f7205f4615c03672df010b1e95bb
Vahe <rubinyan559@gmail.com>
2023-09-29 16:00:45 +0400

    test in head branch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,12 @@ import Container from "../container/Container";
 
 const Header = () => {
   return (
-    <header>
-      <Container>Header</Container>
+    <header className="header">
+      <Container>
+        <div className="header-inner">
+          <li>1</li>
+        </div>
+      </Container>
     </header>
   );
 };
