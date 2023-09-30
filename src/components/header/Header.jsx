37afc9cbb6f076fadd11c072a18e8d0195585333
Vahe <rubinyan559@gmail.com>
2023-09-30 13:44:33 +0400

    Header part

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,43 @@
+//Scss
+import "./Header.scss";
 //Components
+import { Link } from "react-router-dom";
+import { navConfig } from "../../config";
 import Container from "../container/Container";
+import { SlBasket } from "react-icons/sl";
+import { TbCurrencyDram } from "react-icons/tb";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header>
       <Container>
         <div className="header-inner">
-          <li>1</li>
+          <ul className="header-inner-nav">
+            {navConfig.map((link) => {
+              return (
+                <Link
+                  className="header-inner-nav-link"
+                  key={link.id}
+                  to={link.link}
+                >
+                  {link.title.en}
+                </Link>
+              );
+            })}
+          </ul>
+
+          <div className="header-inner-logo">
+            <img src="" alt="logo" className="header-inner-logo-img" />
+          </div>
+          <div className="header-inner-buy">
+            <div className="header-inner-buy-icon">
+              <SlBasket className="header-inner-buy-icon-icn" />
+            </div>
+            <div className="header-inner-buy-cartName">My cart</div>
+            <div className="header-inner-buy-price">
+              0<TbCurrencyDram style={{ width: "18px", height: "18px" }} />
+            </div>
+          </div>
         </div>
       </Container>
     </header>
--- src/config.js
+++ src/config.js
@@ -10,4 +10,31 @@ export const navConfig = [
     },
     link: ROUTER.WATCHES_PAGE_ROUTE,
   },
+  {
+    id: 2,
+    title: {
+      hy: "Երաշխիք",
+      en: "Guarantee",
+      ru: "Гарантия",
+    },
+    link: ROUTER.WATCHES_PAGE_ROUTE,
+  },
+  {
+    id: 3,
+    title: {
+      hy: "Մեր մասին",
+      en: "About us",
+      ru: "О нас",
+    },
+    link: ROUTER.WATCHES_PAGE_ROUTE,
+  },
+  {
+    id: 4,
+    title: {
+      hy: "Կապ մեզ հետ",
+      en: "Contact us",
+      ru: "Связаться с нами",
+    },
+    link: ROUTER.WATCHES_PAGE_ROUTE,
+  },
 ];
