51626f12da485820643592d2784258f737679986
Vahe <rubinyan559@gmail.com>
2023-10-01 23:31:33 +0400

    Slider part 1

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
+import HomeSlider from "../homeSlider/HomeSlider";
 import Watches from "../watches/Watches";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
+      <HomeSlider />
       <Watches />
     </div>
   );
